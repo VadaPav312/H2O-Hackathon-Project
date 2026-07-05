2f1607dabbcbf6e4cd134e9df843af4d9622d60a
Vihaan Gandhi <vihaangandhi11@gmail.com>
2026-07-05 14:42:32 -0700

    Big Update

---
+++ h2o app/h2o app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  h2o app
+//
+//  Created by Vihaan Gandhi on 7/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ h2o app/h2o app/h2o_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  h2o_appApp.swift
+//  h2o app
+//
+//  Created by Vihaan Gandhi on 7/5/26.
+//
+
+import SwiftUI
+
+@main
+struct h2o_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
